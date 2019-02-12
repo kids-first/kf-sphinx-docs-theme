2661912def842c711a35a79d9314f529ff33a8a5
Natasha Singh <dukedesi22@gmail.com>
2019-02-12 17:28:13 -0500

    :tada: Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# kf-sphinx-docs-theme
-💄 Kids First Sphinx Docs Theme
+# Kids First Sphinx Docs Theme
+
+A Kids First styled Sphinx theme based on https://github.com/rtfd/sphinx_rtd_theme
+
+Sphinx is a web documentation generator for Python applications
+
+## Installation
+```
+pip install -e git+https://github.com/kids-first/kf-sphinx-docs-theme.git#egg=kf-sphinx-docs-theme
+```
+
+## Test
+```
+pip install -r dev-requirements.txt
+python -m pytest tests
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+from sphinx_kidsfirst_theme import __version__
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+req_file = os.path.join(root_dir, 'requirements.txt')
+with open(req_file) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='sphinx_kidsfirst_theme',
+    version=__version__,
+    description='Kids First Read the Docs inspired theme for Sphinx',
+    zip_safe=False,
+    packages=['sphinx_kidsfirst_theme'],
+    package_data={'sphinx_kidsfirst_theme': [
+        'theme.conf',
+        'static/css/*.css'
+    ]},
+    include_package_data=True,
+    # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+    entry_points={
+        'sphinx.html_themes': [
+            'sphinx_kidsfirst_theme = sphinx_kidsfirst_theme',
+        ]
+    },
+    install_requires=requirements
+)
